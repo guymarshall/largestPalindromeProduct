dae4e28fb795245099e4bf93ca88765bcf706758
Guy Marshall <guymarshall9@gmail.com>
2022-05-08 11:30:52 +0100

    Remove commented code

--- src/Main.java
+++ src/Main.java
@@ -18,9 +18,6 @@ public class Main {
         stringBuilderReverse.reverse();
 
         return stringBuilderReverse.compareTo(stringBuilder) == 0;
-
-//        String reversedNumberCharacters = reverseString(numberAsString);
-//        return numberAsString.equals(reversedNumberCharacters);
     }
     public static void main(String[] args) {
         System.out.print("Enter a number of digits: ");
@@ -41,37 +38,4 @@ public class Main {
 
         System.out.printf("Maximum palindrome product for %d digits is %d.", userInput, largestPalindrome);
     }
-
-//    public static void main(String[] args) {
-//        System.out.print("Enter a number of digits: ");
-//        Scanner scanner = new Scanner(System.in);
-//        int userInput = scanner.nextInt();
-//        int maxNumber = (int)Math.pow(10, userInput) - 1;
-//
-//        ArrayList<Integer> products = new ArrayList<Integer>();
-//
-//        for (int i = 0; i <= maxNumber; i++) {
-//            for (int j = 0; j <= maxNumber; j++) {
-//                products.add(i * j);
-//            }
-//        }
-//
-//        ArrayList<Integer> palindromes = new ArrayList<Integer>();
-//
-//        for (int product : products) {
-//            if (isPalindrome(product)) {
-//                palindromes.add(product);
-//            }
-//        }
-//
-//        int largestPalindrome = 0;
-//
-//        for (int palindrome : palindromes) {
-//            if (palindrome > largestPalindrome) {
-//                largestPalindrome = palindrome;
-//            }
-//        }
-//
-//        System.out.printf("Maximum palindrome product for %d digits is %d.", userInput, largestPalindrome);
-//    }
 }
